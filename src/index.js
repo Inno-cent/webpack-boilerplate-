20433f31cee2d5c43b9ee6ea23e8fb91ec4f8338
inno-cent <ekweozorinnocent@gmail.com>
2024-10-04 15:38:44 +0100

    new boilerplate push

---
+++ src/index.js
@@ -0,0 +1,26 @@
+// Testing import of a JavaScript module
+import { test } from '@/js/test'
+
+// Testing the import of an asset
+import webpackLogo from '@/images/webpack-logo.svg'
+
+// Testing import of styles
+import '@/styles/index.scss'
+
+// Appending to the DOM
+const logo = document.createElement('img')
+logo.src = webpackLogo
+
+const heading = document.createElement('h1')
+heading.textContent = test()
+
+// Test a background image url in CSS
+const imageBackground = document.createElement('div')
+imageBackground.classList.add('image')
+
+// Test a public folder asset
+const imagePublic = document.createElement('img')
+imagePublic.src = '/assets/example.png'
+
+const app = document.querySelector('#root')
+app.append(logo, heading, imageBackground, imagePublic)
---
+++ src/js/test.js
@@ -0,0 +1,1 @@
+export const test = () => `webpack boilerplate with postcss and babel.`
